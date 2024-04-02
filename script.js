31e826d64e003c6c0396f382c7122e1d0d577153
mmmmaaaril <165331877+mmmmaaaril@users.noreply.github.com>
2024-04-02 13:51:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+// Define the dice
+const dice = {
+    green: [4, 4, 4, 4, 0, 0], // Green die
+    blue: [3, 3, 3, 3, 3, 3], // Blue die
+    red: [2, 2, 2, 2, 6, 6], // Red die (corrected)
+    yellow: [5, 5, 5, 1, 1, 1] // Yellow die
+};
+
+// Function to roll a die
+function rollDie(die) {
+    return die[Math.floor(Math.random() * die.length)];
+}
+
+// Function to play a single game
+function playGame(player1Die, player2Die) {
+    let player1Score = 0;
+    let player2Score = 0;
+
+    // Play 20 rounds
+    for (let i = 0; i < 20; i++) {
+        const player1Roll = rollDie(dice[player1Die]);
+        const player2Roll = rollDie(dice[player2Die]);
+
+        // Determine the winner of the round
+        if (player1Roll > player2Roll) {
+            player1Score++;
+        } else {
+            player2Score++;
+        }
+    }
+
+    // Determine the winner of the game
+    let winner;
+    if (player1Score > player2Score) {
+        winner = "Player 1";
+    } else if (player2Score > player1Score) {
+        winner = "Player 2";
+    } else {
+        winner = "Tie";
+    }
+
+    return { player1Score, player2Score, winner };
+}
+
 // Function to play multiple games
 function playGames(player1Die, player2Die) {
     const game = playGame(player1Die, player2Die);
