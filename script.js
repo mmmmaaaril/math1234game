b43041c3ff409385f99c05664eaff449ca14b194
mmmmaaaril <165331877+mmmmaaaril@users.noreply.github.com>
2024-04-02 13:47:39 -0700

    Update script.js
    select a die

--- script.js
+++ script.js
@@ -1,70 +1,19 @@
-// Define the dice
-const dice = {
-    green: [4, 4, 4, 4, 0, 0], // Green die
-    blue: [3, 3, 3, 3, 3, 3], // Blue die
-    red: [2, 2, 2, 2, 6, 6], // Red die (corrected)
-    yellow: [5, 5, 5, 1, 1, 1] // Yellow die
-};
-
-// Function to roll a die
-function rollDie(die) {
-    return die[Math.floor(Math.random() * die.length)];
-}
-
-// Function to play a single game
-function playGame(player1Die, player2Die) {
-    let player1Score = 0;
-    let player2Score = 0;
-
-    // Play 20 rounds
-    for (let i = 0; i < 20; i++) {
-        const player1Roll = rollDie(dice[player1Die]);
-        const player2Roll = rollDie(dice[player2Die]);
-
-        // Determine the winner of the round
-        if (player1Roll > player2Roll) {
-            player1Score++;
-        } else {
-            player2Score++;
-        }
-    }
-
-    // Determine the winner of the game
-    let winner;
-    if (player1Score > player2Score) {
-        winner = "Player 1";
-    } else if (player2Score > player1Score) {
-        winner = "Player 2";
-    } else {
-        winner = "Tie";
-    }
-
-    return { player1Score, player2Score, winner };
-}
-
 // Function to play multiple games
-function playMultipleGames() {
-    const player1Die = "green";
-    const player2Die = "blue";
-    const games = [playGame(player1Die, player2Die)]; // Play one game with predefined dice
-    return games;
+function playGames(player1Die, player2Die) {
+    const game = playGame(player1Die, player2Die);
+    displayGameResult(game, player1Die, player2Die);
 }
 
-// Function to display game results
-function displayGameResults(games) {
+// Function to display game result
+function displayGameResult(game, player1Die, player2Die) {
     const resultContainer = document.getElementById("game-results");
-    resultContainer.innerHTML = "";
-
-    games.forEach((game, index) => {
-        const gameNumber = index + 1;
-        const gameResultElement = document.createElement("div");
-        gameResultElement.innerHTML = `<b>Game ${gameNumber}:</b> Player 1 (${game.player1Die}) vs Player 2 (${game.player2Die}) - Winner: ${game.winner}, Player 1 Score: ${game.player1Score}, Player 2 Score: ${game.player2Score}`;
-        resultContainer.appendChild(gameResultElement);
-    });
+    resultContainer.innerHTML = `<b>Player 1 Die:</b> ${player1Die}, <b>Player 2 Die:</b> ${player2Die}<br>`;
+    resultContainer.innerHTML += `<b>Winner:</b> ${game.winner}, <b>Player 1 Score:</b> ${game.player1Score}, <b>Player 2 Score:</b> ${game.player2Score}`;
 }
 
 // Update button functionality
 document.getElementById("playButton").addEventListener("click", function() {
-    const games = playMultipleGames();
-    displayGameResults(games);
+    const player1Die = document.getElementById("player1-die").value;
+    const player2Die = document.getElementById("player2-die").value;
+    playGames(player1Die, player2Die);
 });
